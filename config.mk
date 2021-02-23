269b2b19cf48c233a09e949c48d0d8221effe1d7
Marcus <marcus@hardt-it.de>
2021-02-23 11:34:05 +0100

    turn some variables from recursive to simple

--- config.mk
+++ config.mk
@@ -1,12 +1,12 @@
 # user to ssh to
-REMOTE_USER:= "username"
-REMOTE_HOST := "host"
+REMOTE_USER = $(shell username)
+REMOTE_HOST = "specify hostname in config.mk"
 
 # Remote path (relative to remove $HOME)
 REMOTE_DIR := "public_html"
 
 # How can the remove path be reached in a browser?
-REMOTE_URL := "http://${REMOTE_HOST}/${PROJECT}"
+REMOTE_URL = "http://${REMOTE_HOST}/${PROJECT}"
 #REMOTE_URL := "http://<host>/<path>/${PROJECT}"
 
 # we keep that url separate, because that is 18MB. Be sure that additional
@@ -15,6 +15,7 @@ REMOTE_URL := "http://${REMOTE_HOST}/${PROJECT}"
 #
 # You can tweak these, but please be careful (watch the command in
 # the remote-reveal target)
-REVEAL_HOST := ${REMOTE_HOST}
+REVEAL_USER = ${REMOTE_USER}
+REVEAL_HOST = ${REMOTE_HOST}
 REVEAL_DIR  := "public_html/reveal.js"
 REVEAL_URL := "/reveal.js"
